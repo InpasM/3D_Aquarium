2088ce1e8892f0bf27036ed238f0378f95f80838
Mehdi Sapin <mehdi.sapin@gmail.com>
2024-02-28 16:31:04 +0100

    Playing: with movement

--- threeD_Aquarium/threeD_Aquarium/static/main.js
+++ threeD_Aquarium/threeD_Aquarium/static/main.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const appli = document.querySelector('#app');
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 100 );
 const renderer = new THREE.WebGLRenderer();
 
 function initScene() {
@@ -16,15 +16,18 @@ function initScene() {
 }
 
 let aquarium;
+let cube;
 function initObject() {
-	const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	const geometry = new THREE.BoxGeometry( 0.1, 0.4, 1 );
 	const matBox = new THREE.MeshStandardMaterial({
 		color: 0xe9b96e,
 		side: THREE.DoubleSide,
 		roughness: 0.7,
 		metalness: 0.65
 	});
-	const cube = new THREE.Mesh(geometry, matBox);
+	cube = new THREE.Mesh(geometry, matBox);
+	cube.position.set(0, 0.3, 0);
+	cube.castShadow = true;
 	scene.add(cube);
 	
 	// const geoCylinder = new THREE.CylinderGeometry(2, 2, 4, 64);
@@ -53,6 +56,7 @@ function initGround() {
 		side: THREE.DoubleSide
 	});
 	const groundMesh = new THREE.Mesh(ground, groundMaterial);
+	groundMesh.position.set(0, 0, 0);
 	groundMesh.castShadow = false;
 	groundMesh.receiveShadow = true;
 	scene.add(groundMesh);
@@ -86,7 +90,8 @@ function initLights() {
 	scene.add(ambient);
 }
 
-camera.position.set(10, 0, 0);
+var cameraPos = new THREE.Vector3(10, 4, 0);
+camera.position.set(cameraPos.getComponent(0), cameraPos.getComponent(1), cameraPos.getComponent(2));
 camera.lookAt(0, 0, 0);
 
 var movement = {
@@ -111,9 +116,33 @@ function moveObject(obj) {
 	}
 }
 
+// var cameraPos = new THREE.Vector3(10, 10, 0);
+// camera.position.set(cameraPos[0], cameraPos[1], cameraPos[2]);
+function updateCamera() {
+	camera.position.set(cameraPos.getComponent(0), cameraPos.getComponent(1), cameraPos.getComponent(2));
+}
+
+function moveBox() {
+	let translateZValue = (mouseStartPos[0] - mousePos[0]) / 100;
+
+	if (translateZValue > 1)
+		translateZValue = 1;
+	else if (translateZValue < -1)
+		translateZValue = -1;
+
+	// console.log(translateZValue);
+	
+	// if (cube.position.z < 3.2 && cube.position.z > -3.2) {
+		// console.log(cube.position);
+		cube.translateZ(translateZValue);
+	// }
+}
+
 function animate() {
 
 	moveObject(aquarium);
+	// moveBox();
+	// updateCamera();
 	renderer.render( scene, camera );
 	requestAnimationFrame( animate );
 }
@@ -163,8 +192,14 @@ function whichSide(posX, posY) {
 	const midHeight = window.innerHeight / 2;
 	const midWidth = window.innerWidth / 2;
 
-	if (posX <= midWidth && posY <= midHeight)
+	if (posX <= midWidth && posY <= midHeight) {
+		// camera.rotateY(0.001);
+		// camera.translateY(0.01);
+		// camera.lookAt(2, 0.1, 0);
+		// camera.position()
+		// cameraPos.setComponent(0, cameraPos.getComponent(0) + 0.01);
 		return TOPLEFT;
+	}
 	else if (posX > midWidth && posY <= midHeight)
 		return TOPRIGHT;
 	else if (posX <= midWidth && posY > midHeight)
@@ -173,18 +208,29 @@ function whichSide(posX, posY) {
 		return BOTTOMRIGHT;
 }
 
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-document.addEventListener("mousemove", function(e) {
+document.addEventListener("click", startGame);
 
-	mousePosX.innerText = "x: " + e.clientX + " / " + windowWidth;
-	mousePosY.innerText = "y: " + e.clientY + " / " + windowHeight;
-	mouseSide.innerText = "side: " + whichSide(e.clientX, e.clientY);
+var mouseStartPos = [0, 0];
+var mousePos = [0, 0];
+var startup = true;
+function startGame() {
+	document.removeEventListener("click", startGame);
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
 
+	document.addEventListener("mousemove", function(e) {
+		if (startup) {
+			mouseStartPos = [e.clientX, e.clientY];
+			startup = false;
+		}
+		mousePos = [e.clientX, e.clientY];
+		mousePosX.innerText = "x: " + e.clientX + " / " + mouseStartPos[0];
+		mousePosY.innerText = "y: " + e.clientY + " / " + mouseStartPos[1];
+		// mouseSide.innerText = "side: " + whichSide(e.clientX, e.clientY);
+	});
+}
 
-});
-
-document.body.style.cursor = "none";
+// document.body.style.cursor = "none";
 initScene();
 initObject();
 initGround();
